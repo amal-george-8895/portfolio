4181128803c5ac0cd92b009857aad10a7ef2f760
AMAL GEORGE <142260570+amal-george-8895@users.noreply.github.com>
2024-07-05 10:07:49 +0100

    Delete travel porfolio/app.js

--- travel porfolio/app.js
+++
@@ -1,5 +0,0 @@
-const readmorebtn = document.querySelector("readmore-btn");
-const readmorediv = document.querySelector("readmore-div");
-readmorebtn.addEventListener("click", () => {
-  readmorediv.classList.toggle("readmore-div");
-});
